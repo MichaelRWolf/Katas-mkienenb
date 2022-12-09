b6cae495be6b3dfc59f5ed8947b8553ec3575e14
Mike Kienenberger <mkienenb@gmail.com>
2022-12-09 15:57:53 +0000

    Sample Rust Fizbuzz

---
+++ main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use std::iter;
+
+fn fizz_buzz(numbers: Vec<i32>) -> Vec<String> {
+    numbers.iter().map(|i| {
+        if *i % 15 == 0 {
+            "Fizz Buzz".to_string()
+        } else if *i % 5 == 0 {
+            "Buzz".to_string()
+        } else if *i % 3 == 0 {
+            "Fizz".to_string()
+        } else {
+            i.to_string()
+        }
+    }).collect()
+ }
+
+ fn main() {
+    // Get user input
+    println!("Enter a number:");
+ 
+    let mut input = String::new();
+ 
+    io::stdin().read_line(&mut input);
+ 
+    let input: i32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    };
+ 
+    // Create a range from 1 to the user's input
+    let numbers = (1..=input).collect::<Vec<i32>>();
+ 
+    // Run fizz_buzz function
+    let result = fizz_buzz(numbers);
+ 
+    // Print result
+    for n in result {
+        println!("{}", n);
+    }
+ }
