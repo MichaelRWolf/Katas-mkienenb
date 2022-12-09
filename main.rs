18538ed67e573ecb177b19bb722a896337ef626b
Michael R Wolf <MichaelRWolf@att.net>
2022-12-09 21:12:01 +0000

    Pass - Compiler warnings/errors eliminated

--- main.rs
+++ main.rs
@@ -1,5 +1,4 @@
 use std::io;
-use std::iter;
 
 fn fizz_buzz(numbers: Vec<i32>) -> Vec<String> {
     numbers.iter().map(|i| {
@@ -21,7 +20,7 @@ fn fizz_buzz(numbers: Vec<i32>) -> Vec<String> {
  
     let mut input = String::new();
  
-    io::stdin().read_line(&mut input);
+    let _err = io::stdin().read_line(&mut input);
  
     let input: i32 = match input.trim().parse() {
         Ok(num) => num,
